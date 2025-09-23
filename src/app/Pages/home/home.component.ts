a821bb0d3a5a9a51c840302768f97ebfcd019870
Mauricio Bochatay <bochataymauricio01@gmail.com>
2025-09-22 21:42:36 -0300

    add reusable cards for products

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CarruselComponent } from "../../Components/carrusel/carrusel.component";
+import { CardComponent } from "../../Components/card/card.component";
 
 @Component({
   selector: 'app-home',
-  imports: [CarruselComponent],
+  imports: [CarruselComponent, CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
