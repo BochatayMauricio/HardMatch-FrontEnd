fbd04851b2a784253b7140f7ecec64c979c316cd
valentinomariani9 <valentinomariani9@gmail.com>
2026-01-16 09:52:25 -0300

    Ajustes en el login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { NavbarComponent } from "./Components/navbar/navbar.component";
 import { FooterComponent } from "./Components/footer/footer.component";
+import { NavbarLoginComponent } from './Components/navbar-login/navbar-login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'hardmatch-frontend';
+  showMainHeader: boolean = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const hiddenRoutes = ['/login'];
+        this.showMainHeader = !hiddenRoutes.includes(event.urlAfterRedirects);
+      }
+    });
+  }
 }
